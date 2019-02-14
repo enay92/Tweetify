7be7dab7ecde0ba2e987527469ddc29e60c11f91
Eni SINANI <eni.sinani@estiam.com>
2019-02-14 13:35:37 +0100

    followrs ok

---
+++ Tweetify/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tweetify.DAL;
+using Tweetify.Models;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tweetify.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(User u)
+        {
+            using (var context = new TweetifyContext())
+            {
+                User temp = context.Users.FirstOrDefault(x => x.Username == u.Username);
+
+                if (temp != null)
+                {
+                    throw new Exception("User all ready exists");
+                }
+                else
+                {
+                    u.Username = u.Username.ToLower();
+                    context.Users.Add(u);
+                    await context.SaveChangesAsync();
+                    return RedirectToAction("Login");
+                }
+            }
+        }
+
+        public IActionResult Login()
+        {
+            // Récupérer un user à partir du username (en minuscules)
+            // SI User => Verifier Mdp 
+            // // Si User.MDP == MDP 
+            // //    Je créée la variable de session
+            // // SINON
+            // //   STFU
+            // SINON 
+            //   STFU 
+
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            // VARIABLE DE SESSION ? DIE MUTHAFUKA DIE !
+            return View();
+        }
+    }
+}
---
+++ Tweetify/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tweetify.Models;
+
+namespace Tweetify.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("Username", "Bob");
+            HttpContext.Session.GetString("Username");
+
+            HttpContext.Session.SetInt32("UserId", 3);
+            HttpContext.Session.GetInt32("UserId");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
